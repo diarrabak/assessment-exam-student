a90fe0c5159629975580b2df0163b17a9c4f0714
diarrabak <71572448+diarrabak@users.noreply.github.com>
2021-11-20 22:40:29 +0000

    adding the mode calculation

--- answers/03.js
+++ answers/03.js
@@ -13,12 +13,40 @@
  *
  *    6
  */
-const mode = function(arr) {
+const mode = function (arr) {
+  const values = uniContent(arr); //Unique numbers of the array
+  const modes = []; //All the modes
+  for (let i = 0; i < values.length; i++) {
+    for (let value of arr) {
+      if (value === values[i]) {
+        //Loop on the input array and compare all elements to each unique value record the occurrence
+        if (modes[i]) {
+          //If mode not empty
+          modes[i] += 1; //increment
+        } else {
+          modes[i] = 1; //Otherwise set to 1
+        }
+      }
+    }
+  }
 
+  return values[modes.indexOf(Math.max.apply(null, modes))];
+  // return modes;
 };
 
-
-
+//Find the different numbers forming the arrays without duplicates
+const uniContent = function (array) {
+  const occurences = [];
+  for (let element of array) {
+    //Loop on input array
+    if (occurences.indexOf(element) < 0) {
+      //Add the current element only if it doesn't exist in occurrences (only once)
+      occurences.push(element);
+    }
+  }
+  return occurences;
+};
+// console.log(mode([6, 2, 3, 4, 2, 9, 2,6, 1, 0, 5]));
 // Don't change below:
 
 module.exports = { mode };
